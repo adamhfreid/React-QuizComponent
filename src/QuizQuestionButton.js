1fdd3465885cc756a5454c06dd27c1dde18fb3c1
Adam Freid <adam.h.freid@accenture.com>
2019-02-19 11:31:36 -0600

    Added quiz button

---
+++ src/QuizQuestionButton.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+let quizData = require('./quiz_data.json')
+
+class QuizQuestionButton extends Component {
+  constructor(props){
+    super(props)
+  }
+  render() {
+    return (
+      <li><button>{this.props.button_text}</button></li>
+    )
+  }
+}
+
+export default QuizQuestionButton;
